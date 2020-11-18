6705bb56d95720ba31f6e0df8b1b7f5a5812a4c5
Vic <example@gmail.com>
2020-11-18 11:59:32 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// let number = 4.6;
+
+// console.log(4/0);
+
+// const obj = {
+//     name: "Vic",
+//     age: 28,
+//     isMarried: false
+// };
+// console.log(obj.name);
+
+// let arr = ['plumb', 'orange', 'apple'];
+// console.log(arr[1], arr[2]);
+
+// const result = confirm("Are you hire?");
+// console.log(result);
+
+// const answer = prompt("are you 18?", "")
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// // answers[0] = prompt('your name?', '');
+// // answers[1] = prompt('your age?', '');
+// // answers[2] = prompt('your sex?', '');
+
+// console.log(typeof(answers));
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Vic";
+
+alert(`hello, ${user}`);
